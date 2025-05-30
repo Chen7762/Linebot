c2a30bc06404cc6ef0540e569e9ee5941e47ae0d
Chen7762 <a035421648@gmail.com>
2025-05-31 00:40:40 +0800

    fix: 改用環境變數 PORT 解決 Render 啟動問題

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/posts/:category', (req, res) => {
 
 
 // 啟動伺服器
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`伺服器啟動：http://localhost:${PORT}`);
 });
